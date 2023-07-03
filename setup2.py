7ec421cb2c540af351317222d067d41a43a5585f
paulo.campos <paulo.campos@minervafoods.com>
2023-07-03 00:44:37 -0300

    Setup.py novo

---
+++ setup2.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+def get_requirements(file_path):
+    with open(file_path, 'r') as file:
+        requirements = file.read().splitlines()
+    return [str(req) for req in parse_requirements(requirements)]
+
+requirements = get_requirements('/home/paulo/Authenticator/requirements.txt')
+
+setup(
+    name='Authenticator',
+    version='1.0',
+    author='Seu Nome',
+    author_email='paulohenriqueh178h@gmail.com',
+    description='Programa para autenticação de dois fatores',
+    packages=find_packages(),
+    install_requires=requirements,
+    long_description='Programa para autenticação de dois fatores'
+)
